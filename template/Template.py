6bb9a6017e8eb2668547bc95c3f4a48081ab6ad8
laialaodi <776273308@qq.com>
2025-07-25 17:50:02 +0800

    导入template

---
+++ template/Template.py
@@ -0,0 +1,19 @@
+#!/bin/env python3
+# -*- coding: UTF-8 -*-
+import numpy
+from manim import *
+
+from sys import path
+
+path.append("C:/Users/frank/Documents/E/pg/Python/Manimations/")
+
+from std import *
+
+
+class TBD(TemplateScene):
+    def construct(self):
+        pass
+
+
+if __name__ == "__main__":
+    render(__file__)
